f611890b3fbdd72c5a5892a4b71cfa07d3f3e98b
onepercman <onepercman@gmail.com>
2025-06-17 11:56:39 +0700

    🚀 update lint

--- packages/eslint-config/react.mjs
+++ packages/eslint-config/react.mjs
@@ -1,1 +1,30 @@
-export default [{}];
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    name: "custom",
+    settings: { react: { version: "detect" } },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { ignoreRestSiblings: true },
+      ],
+      "@typescript-eslint/no-unused-expressions": ["warn"],
+      "react/prop-types": ["off"],
+      "react/display-name": ["warn"],
+    },
+  },
+  eslintConfigPrettier,
+];
